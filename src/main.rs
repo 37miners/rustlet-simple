b18f09a9818876c029d4ec701ae87abc389a6011
Snape 479 <Severus_Snape93@protonmail.com>
2021-08-17 23:39:12 -0700

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use librustlet::*;
-log::debug!();
+use librustlet::*; // use the librustlet library
+log::debug!(); // set log level to debug
 
 fn main() {
-	rustlet_init!(RustletConfig::default());
-	rustlet!("myrustlet", { response!("Hello World!"); });
-	rustlet_mapping!("/", "myrustlet");
-	std::thread::park();
+	rustlet_init!(RustletConfig::default()); // initialize with the default config
+	rustlet!("myrustlet", { response!("Hello World!"); }); // hello world rustlet
+	rustlet_mapping!("/", "myrustlet"); // create mapping to '/'
+	std::thread::park(); // park the thread so we don't exit
 }
